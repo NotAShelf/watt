bc343eefd9cb40245e3e5cfe631a875c70c14fdb
RGBCube <git@rgbcu.be>
2025-05-19 16:17:54 +0300

    power_supply&cpu: use objects

--- src/power_supply.rs
+++ src/power_supply.rs
@@ -1,10 +1,21 @@
-use anyhow::Context;
+use anyhow::{Context, bail};
 
 use std::{
     fmt, fs,
-    os::macos::fs::MetadataExt,
     path::{Path, PathBuf},
 };
+
+// TODO: Migrate to central utils file. Same exists in cpu.rs.
+fn write(path: impl AsRef<Path>, value: &str) -> anyhow::Result<()> {
+    let path = path.as_ref();
+
+    fs::write(path, value).with_context(|| {
+        format!(
+            "failed to write '{value}' to '{path}'",
+            path = path.display(),
+        )
+    })
+}
 
 /// Represents a pattern of path suffixes used to control charge thresholds
 /// for different device vendors.
@@ -50,7 +61,12 @@ pub struct PowerSupply {
 
 impl fmt::Display for PowerSupply {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "power supply '{name}'", name = &self.name)?;
+        write!(
+            f,
+            "power supply '{name}' at '{path}'",
+            name = &self.name,
+            path = self.path.display(),
+        )?;
 
         if let Some(config) = self.threshold_config.as_ref() {
             write!(
@@ -64,7 +80,93 @@ impl fmt::Display for PowerSupply {
     }
 }
 
+const POWER_SUPPLY_PATH: &str = "/sys/class/power_supply";
+
 impl PowerSupply {
+    pub fn from_name(name: String) -> anyhow::Result<Self> {
+        let mut power_supply = Self {
+            path: Path::new(POWER_SUPPLY_PATH).join(&name),
+            name,
+            threshold_config: None,
+        };
+
+        power_supply.rescan()?;
+
+        Ok(power_supply)
+    }
+
+    pub fn from_path(path: PathBuf) -> anyhow::Result<Self> {
+        let mut power_supply = PowerSupply {
+            name: path
+                .file_name()
+                .with_context(|| {
+                    format!("failed to get file name of '{path}'", path = path.display(),)
+                })?
+                .to_string_lossy()
+                .to_string(),
+
+            path,
+
+            threshold_config: None,
+        };
+
+        power_supply.rescan()?;
+
+        Ok(power_supply)
+    }
+
+    pub fn all() -> anyhow::Result<Vec<PowerSupply>> {
+        let mut power_supplies = Vec::new();
+
+        for entry in fs::read_dir(POWER_SUPPLY_PATH)
+            .with_context(|| format!("failed to read '{POWER_SUPPLY_PATH}'"))?
+        {
+            let entry = match entry {
+                Ok(entry) => entry,
+
+                Err(error) => {
+                    log::warn!("failed to read power supply entry: {error}");
+                    continue;
+                }
+            };
+
+            power_supplies.push(PowerSupply::from_path(entry.path())?);
+        }
+
+        Ok(power_supplies)
+    }
+
+    fn is_battery(&self) -> anyhow::Result<bool> {
+        let type_path = self.path.join("type");
+
+        let type_ = fs::read_to_string(&type_path)
+            .with_context(|| format!("failed to read '{path}'", path = type_path.display()))?;
+
+        Ok(type_ == "Battery")
+    }
+
+    pub fn rescan(&mut self) -> anyhow::Result<()> {
+        let threshold_config = self
+            .is_battery()
+            .with_context(|| format!("failed to determine what type of power supply '{self}' is"))?
+            .then(|| {
+                for config in POWER_SUPPLY_THRESHOLD_CONFIGS {
+                    if self.path.join(config.path_start).exists()
+                        && self.path.join(config.path_end).exists()
+                    {
+                        return Some(*config);
+                    }
+                }
+
+                None
+            })
+            .flatten();
+
+        self.threshold_config = threshold_config;
+
+        Ok(())
+    }
+
     pub fn charge_threshold_path_start(&self) -> Option<PathBuf> {
         self.threshold_config
             .map(|config| self.path.join(config.path_start))
@@ -74,155 +176,74 @@ impl PowerSupply {
         self.threshold_config
             .map(|config| self.path.join(config.path_end))
     }
-}
 
-// TODO: Migrate to central utils file. Same exists in cpu.rs.
-fn write(path: impl AsRef<Path>, value: &str) -> anyhow::Result<()> {
-    let path = path.as_ref();
-
-    fs::write(path, value).with_context(|| {
-        format!(
-            "failed to write '{value}' to '{path}'",
-            path = path.display(),
+    pub fn set_charge_threshold_start(&self, charge_threshold_start: u8) -> anyhow::Result<()> {
+        write(
+            &self.charge_threshold_path_start().ok_or_else(|| {
+                anyhow::anyhow!(
+                    "power supply '{name}' does not support changing charge threshold levels",
+                    name = self.name,
+                )
+            })?,
+            &charge_threshold_start.to_string(),
         )
-    })
-}
+        .with_context(|| format!("failed to set charge threshold start for {self}"))?;
 
-fn is_battery(path: &Path) -> anyhow::Result<bool> {
-    let type_path = path.join("type");
+        log::info!("set battery threshold start for {self} to {charge_threshold_start}%");
 
-    let type_ = fs::read_to_string(&type_path)
-        .with_context(|| format!("failed to read '{path}'", path = type_path.display()))?;
-
-    Ok(type_ == "Battery")
-}
-
-const POWER_SUPPLY_PATH: &str = "/sys/class/power_supply";
-
-/// Get power supply.
-pub fn get_power_supply(name: &str) -> anyhow::Result<PowerSupply> {
-    let entry_path = Path::new(POWER_SUPPLY_PATH).join(name);
-
-    let threshold_config = is_battery(&entry_path)
-        .with_context(|| {
-            format!(
-                "failed to determine what type of power supply '{path}' is",
-                path = entry_path.display(),
-            )
-        })?
-        .then(|| {
-            for config in POWER_SUPPLY_THRESHOLD_CONFIGS {
-                if entry_path.join(config.path_start).exists()
-                    && entry_path.join(config.path_end).exists()
-                {
-                    return Some(*config);
-                }
-            }
-
-            None
-        })
-        .flatten();
-
-    Ok(PowerSupply {
-        name: name.to_owned(),
-        path: entry_path,
-        threshold_config,
-    })
-}
-
-/// Get all power supplies.
-pub fn get_power_supplies() -> anyhow::Result<Vec<PowerSupply>> {
-    let mut power_supplies = Vec::new();
-
-    for entry in fs::read_dir(POWER_SUPPLY_PATH)
-        .with_context(|| format!("failed to read '{POWER_SUPPLY_PATH}'"))?
-    {
-        let entry = match entry {
-            Ok(entry) => entry,
-
-            Err(error) => {
-                log::warn!("failed to read power supply entry: {error}");
-                continue;
-            }
-        };
-
-        let entry_path = entry.path();
-
-        let mut power_supply_config = None;
-
-        if is_battery(&entry_path).with_context(|| {
-            format!(
-                "failed to determine what type of power supply '{path}' is",
-                path = entry_path.display(),
-            )
-        })? {
-            for config in POWER_SUPPLY_THRESHOLD_CONFIGS {
-                if entry_path.join(config.path_start).exists()
-                    && entry_path.join(config.path_end).exists()
-                {
-                    power_supply_config = Some(*config);
-                    break;
-                }
-            }
-        }
-
-        power_supplies.push(PowerSupply {
-            name: entry_path
-                .file_name()
-                .with_context(|| {
-                    format!(
-                        "failed to get file name of '{path}'",
-                        path = entry_path.display(),
-                    )
-                })?
-                .to_string_lossy()
-                .to_string(),
-
-            path: entry_path,
-
-            threshold_config: power_supply_config,
-        });
+        Ok(())
     }
 
-    Ok(power_supplies)
-}
+    pub fn set_charge_threshold_end(&self, charge_threshold_end: u8) -> anyhow::Result<()> {
+        write(
+            &self.charge_threshold_path_end().ok_or_else(|| {
+                anyhow::anyhow!(
+                    "power supply '{name}' does not support changing charge threshold levels",
+                    name = self.name,
+                )
+            })?,
+            &charge_threshold_end.to_string(),
+        )
+        .with_context(|| format!("failed to set charge threshold end for {self}"))?;
 
-pub fn set_charge_threshold_start(
-    power_supply: &PowerSupply,
-    charge_threshold_start: u8,
-) -> anyhow::Result<()> {
-    write(
-        &power_supply.charge_threshold_path_start().ok_or_else(|| {
-            anyhow::anyhow!(
-                "power supply '{name}' does not support changing charge threshold levels",
-                name = power_supply.name,
-            )
-        })?,
-        &charge_threshold_start.to_string(),
-    )
-    .with_context(|| format!("failed to set charge threshold start for {power_supply}"))?;
+        log::info!("set battery threshold end for {self} to {charge_threshold_end}%");
 
-    log::info!("set battery threshold start for {power_supply} to {charge_threshold_start}%");
+        Ok(())
+    }
 
-    Ok(())
-}
+    pub fn get_available_platform_profiles() -> Vec<String> {
+        let path = "/sys/firmware/acpi/platform_profile_choices";
 
-pub fn set_charge_threshold_end(
-    power_supply: &PowerSupply,
-    charge_threshold_end: u8,
-) -> anyhow::Result<()> {
-    write(
-        &power_supply.charge_threshold_path_end().ok_or_else(|| {
-            anyhow::anyhow!(
-                "power supply '{name}' does not support changing charge threshold levels",
-                name = power_supply.name,
-            )
-        })?,
-        &charge_threshold_end.to_string(),
-    )
-    .with_context(|| format!("failed to set charge threshold end for {power_supply}"))?;
+        let Ok(content) = fs::read_to_string(path) else {
+            return Vec::new();
+        };
 
-    log::info!("set battery threshold end for {power_supply} to {charge_threshold_end}%");
+        content
+            .split_whitespace()
+            .map(ToString::to_string)
+            .collect()
+    }
 
-    Ok(())
+    /// Sets the platform profile.
+    /// This changes the system performance, temperature, fan, and other hardware replated characteristics.
+    ///
+    /// Also see [`The Kernel docs`] for this.
+    ///
+    /// [`The Kernel docs`]: <https://docs.kernel.org/userspace-api/sysfs-platform_profile.html>
+    pub fn set_platform_profile(profile: &str) -> anyhow::Result<()> {
+        let profiles = Self::get_available_platform_profiles();
+
+        if !profiles
+            .iter()
+            .any(|avail_profile| avail_profile == profile)
+        {
+            bail!(
+                "profile '{profile}' is not available for system. valid profiles: {profiles}",
+                profiles = profiles.join(", "),
+            );
+        }
+
+        write("/sys/firmware/acpi/platform_profile", profile)
+            .context("this probably means that your system does not support changing ACPI profiles")
+    }
 }
