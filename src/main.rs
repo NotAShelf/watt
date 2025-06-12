e9e1df90e6d3f263143168ae8d7400ca83469631
RGBCube <git@rgbcu.be>
2025-06-12 20:19:40 +0300

    treewide: toml and rust formatter changes

--- src/main.rs
+++ src/main.rs
@@ -10,143 +10,149 @@ mod daemon;
 // mod engine;
 // mod monitor;
 
+use std::{
+  fmt::Write as _,
+  io,
+  io::Write as _,
+  path::PathBuf,
+  process,
+};
+
 use anyhow::Context;
 use clap::Parser as _;
-use std::fmt::Write as _;
-use std::io::Write as _;
-use std::path::PathBuf;
-use std::{io, process};
 use yansi::Paint as _;
 
 #[derive(clap::Parser, Debug)]
 #[clap(author, version, about)]
 struct Cli {
-    #[clap(subcommand)]
-    command: Command,
+  #[clap(subcommand)]
+  command: Command,
 }
 
 #[derive(clap::Parser, Debug)]
 #[clap(multicall = true)]
 enum Command {
-    /// Watt daemon.
-    Watt {
-        #[command(flatten)]
-        verbosity: clap_verbosity_flag::Verbosity,
+  /// Watt daemon.
+  Watt {
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
 
-        /// The daemon config path.
-        #[arg(long, env = "WATT_CONFIG")]
-        config: Option<PathBuf>,
-    },
+    /// The daemon config path.
+    #[arg(long, env = "WATT_CONFIG")]
+    config: Option<PathBuf>,
+  },
 
-    /// CPU metadata and modification utility.
-    Cpu {
-        #[command(flatten)]
-        verbosity: clap_verbosity_flag::Verbosity,
+  /// CPU metadata and modification utility.
+  Cpu {
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
 
-        #[clap(subcommand)]
-        command: CpuCommand,
-    },
+    #[clap(subcommand)]
+    command: CpuCommand,
+  },
 
-    /// Power supply metadata and modification utility.
-    Power {
-        #[command(flatten)]
-        verbosity: clap_verbosity_flag::Verbosity,
+  /// Power supply metadata and modification utility.
+  Power {
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
 
-        #[clap(subcommand)]
-        command: PowerCommand,
-    },
+    #[clap(subcommand)]
+    command: PowerCommand,
+  },
 }
 
 #[derive(clap::Parser, Debug)]
 enum CpuCommand {
-    /// Modify CPU attributes.
-    Set(config::CpuDelta),
+  /// Modify CPU attributes.
+  Set(config::CpuDelta),
 }
 
 #[derive(clap::Parser, Debug)]
 enum PowerCommand {
-    /// Modify power supply attributes.
-    Set(config::PowerDelta),
+  /// Modify power supply attributes.
+  Set(config::PowerDelta),
 }
 
 fn real_main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
+  let cli = Cli::parse();
 
-    yansi::whenever(yansi::Condition::TTY_AND_COLOR);
+  yansi::whenever(yansi::Condition::TTY_AND_COLOR);
 
-    let (Command::Watt { verbosity, .. }
-    | Command::Cpu { verbosity, .. }
-    | Command::Power { verbosity, .. }) = cli.command;
+  let (Command::Watt { verbosity, .. }
+  | Command::Cpu { verbosity, .. }
+  | Command::Power { verbosity, .. }) = cli.command;
 
-    env_logger::Builder::new()
-        .filter_level(verbosity.log_level_filter())
-        .format_timestamp(None)
-        .format_module_path(false)
-        .init();
+  env_logger::Builder::new()
+    .filter_level(verbosity.log_level_filter())
+    .format_timestamp(None)
+    .format_module_path(false)
+    .init();
 
-    match cli.command {
-        Command::Watt { config, .. } => {
-            let config = config::DaemonConfig::load_from(config.as_deref())
-                .context("failed to load daemon config")?;
+  match cli.command {
+    Command::Watt { config, .. } => {
+      let config = config::DaemonConfig::load_from(config.as_deref())
+        .context("failed to load daemon config")?;
 
-            daemon::run(config)
-        }
+      daemon::run(config)
+    },
 
-        Command::Cpu {
-            command: CpuCommand::Set(delta),
-            ..
-        } => delta.apply(),
+    Command::Cpu {
+      command: CpuCommand::Set(delta),
+      ..
+    } => delta.apply(),
 
-        Command::Power {
-            command: PowerCommand::Set(delta),
-            ..
-        } => delta.apply(),
-    }
+    Command::Power {
+      command: PowerCommand::Set(delta),
+      ..
+    } => delta.apply(),
+  }
 }
 
 fn main() {
-    let Err(error) = real_main() else {
-        return;
+  let Err(error) = real_main() else {
+    return;
+  };
+
+  let mut err = io::stderr();
+
+  let mut message = String::new();
+  let mut chain = error.chain().rev().peekable();
+
+  while let Some(error) = chain.next() {
+    let _ = write!(
+      err,
+      "{header} ",
+      header = if chain.peek().is_none() {
+        "error:"
+      } else {
+        "cause:"
+      }
+      .red()
+      .bold(),
+    );
+
+    String::clear(&mut message);
+    let _ = write!(message, "{error}");
+
+    let mut chars = message.char_indices();
+
+    let _ = match (chars.next(), chars.next()) {
+      (Some((_, first)), Some((second_start, second)))
+        if second.is_lowercase() =>
+      {
+        writeln!(
+          err,
+          "{first_lowercase}{rest}",
+          first_lowercase = first.to_lowercase(),
+          rest = &message[second_start..],
+        )
+      },
+
+      _ => {
+        writeln!(err, "{message}")
+      },
     };
+  }
 
-    let mut err = io::stderr();
-
-    let mut message = String::new();
-    let mut chain = error.chain().rev().peekable();
-
-    while let Some(error) = chain.next() {
-        let _ = write!(
-            err,
-            "{header} ",
-            header = if chain.peek().is_none() {
-                "error:"
-            } else {
-                "cause:"
-            }
-            .red()
-            .bold(),
-        );
-
-        String::clear(&mut message);
-        let _ = write!(message, "{error}");
-
-        let mut chars = message.char_indices();
-
-        let _ = match (chars.next(), chars.next()) {
-            (Some((_, first)), Some((second_start, second))) if second.is_lowercase() => {
-                writeln!(
-                    err,
-                    "{first_lowercase}{rest}",
-                    first_lowercase = first.to_lowercase(),
-                    rest = &message[second_start..],
-                )
-            }
-
-            _ => {
-                writeln!(err, "{message}")
-            }
-        };
-    }
-
-    process::exit(1);
+  process::exit(1);
 }
