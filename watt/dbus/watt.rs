6c048ba8d184421679d4f4a6abe219ee76661cff
NotAShelf <raf@notashelf.dev>
2026-02-03 19:12:28 +0300

    watt: initial d-bus interface work
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I75f55319b2d76636bdab8b02923fa3ff6a6a6964

---
+++ watt/dbus/watt.rs
@@ -0,0 +1,72 @@
+use std::{
+  collections::HashMap,
+  sync::Arc,
+};
+
+use tokio::sync::RwLock;
+use zbus::{
+  interface,
+  zvariant::Value,
+};
+
+use crate::system::DaemonState;
+
+pub struct WattInterface {
+  state: Arc<RwLock<DaemonState>>,
+}
+
+impl WattInterface {
+  pub fn new(state: Arc<RwLock<DaemonState>>) -> Self {
+    Self { state }
+  }
+}
+
+#[interface(name = "dev.notashelf.Watt")]
+impl WattInterface {
+  #[zbus(property)]
+  async fn version(&self) -> String {
+    env!("CARGO_PKG_VERSION").to_owned()
+  }
+
+  #[zbus(property)]
+  async fn rule_count(&self) -> u32 {
+    let state = self.state.read().await;
+    state.rule_count() as u32
+  }
+
+  #[zbus(property)]
+  async fn cpu_count(&self) -> u32 {
+    let state = self.state.read().await;
+    state.cpu_count() as u32
+  }
+
+  async fn get_status(&self) -> HashMap<String, Value<'_>> {
+    let state = self.state.read().await;
+    let mut status = HashMap::new();
+
+    if let Some(log) = state.latest_cpu_log() {
+      status.insert("cpu-usage".to_owned(), Value::from(log.usage * 100.0));
+
+      if let Some(temperature) = log.temperature {
+        status.insert("cpu-temperature".to_owned(), Value::from(temperature));
+      }
+    }
+
+    status.insert(
+      "profile".to_owned(),
+      Value::from(String::from(state.active_profile().as_str())),
+    );
+
+    status.insert(
+      "is-discharging".to_owned(),
+      Value::from(state.is_discharging()),
+    );
+
+    status
+  }
+
+  async fn get_applied_rules(&self) -> Vec<String> {
+    let state = self.state.read().await;
+    state.last_applied_rules()
+  }
+}
