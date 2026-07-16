6eca7968cff7027086d35463a425dd1510c2bc95
NotAShelf <raf@notashelf.dev>
2026-07-16 16:54:15 +0300

    nix: streamline NixOS module based on Nixpkgs module feedback
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: Id8ea2ceb4241a88926ff7552165f42976a6a6964

--- nix/module.nix
+++ nix/module.nix
@@ -7,29 +7,38 @@ inputs: {
   inherit (lib.modules) mkIf;
   inherit (lib.options) mkOption mkEnableOption mkPackageOption;
   inherit (lib.types) submodule;
-  inherit (lib.lists) optional;
   inherit (lib.meta) getExe;
 
   cfg = config.services.watt;
 
   format = pkgs.formats.toml {};
   cfgFile = format.generate "watt-config.toml" cfg.settings;
+
+  conflictingServices = [
+    "power-profiles-daemon"
+    "auto-cpufreq"
+    "tlp"
+    "cpupower-gui"
+    "thermald"
+    "tuned"
+  ];
 in {
   options.services.watt = {
-    enable = mkEnableOption "Automatic CPU speed & power optimizer for Linux";
-    package = mkPackageOption inputs.self.packages.${pkgs.stdenv.system} "watt" {
-      pkgsText = "self.packages.\${pkgs.stdenv.system}";
+    enable = mkEnableOption "Watt, automatic CPU speed & power optimizer for Linux";
+    package = mkPackageOption inputs.self.packages.${pkgs.stdenv.hostPlatform.system} "watt" {
+      pkgsText = "self.packages.\${pkgs.stdenv.hostPlatform.system}";
     };
 
     settings = mkOption {
-      default = {};
       type = submodule {freeformType = format.type;};
+      default = {};
       description = "Configuration for Watt.";
     };
   };
 
   config = mkIf cfg.enable {
     environment.systemPackages = [cfg.package];
+    services.dbus.packages = [cfg.package];
 
     # This is necessary for the Watt CLI. The environment variable
     # passed to the systemd service will take priority in read order.
@@ -37,15 +46,8 @@ in {
 
     systemd.services.watt = {
       wantedBy = ["multi-user.target"];
-      conflicts = [
-        "auto-cpufreq.service"
-        "power-profiles-daemon.service"
-        "tlp.service"
-        "cpupower-gui.service"
-        "thermald.service"
-      ];
+      conflicts = map (service: "${service}.service") conflictingServices;
       serviceConfig = {
-        Environment = optional (cfg.settings != {}) ["WATT_CONFIG=${cfgFile}"];
         WorkingDirectory = "";
         ExecStart = getExe cfg.package;
         Restart = "on-failure";
@@ -55,21 +57,11 @@ in {
       };
     };
 
-    assertions = [
-      {
-        assertion = !config.services.power-profiles-daemon.enable;
-        message = ''
-          You have set services.power-profiles-daemon.enable = true;
-          which conflicts with Watt.
-        '';
-      }
-      {
-        assertion = !config.services.auto-cpufreq.enable;
-        message = ''
-          You have set services.auto-cpufreq.enable = true;
-          which conflicts with Watt.
-        '';
-      }
-    ];
+    assertions =
+      map (service: {
+        assertion = !config.services.${service}.enable;
+        message = "You have set services.${service}.enable = true; which conflicts with Watt.";
+      })
+      conflictingServices;
   };
 }
